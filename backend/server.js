6913a30b6f2ef6f4e7eb6b70437ac514ddcc72c2
陳柏元 <c_boyuan@chenbaiyuandeMacBook-Pro.local>
2023-07-10 00:32:57 -0700

    Yuan

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,9 @@ const app = express()
 const path = require('path')
 
 
-app.use('/', express.static(path.join(__dirname, '/backend/public')))
+app.get("/", function(req, res) {
+    res.send("Hello world");
+});
 
 app.listen(3000, function() {
     console.log("server run on port 3000.");
