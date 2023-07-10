87a3d429b7af4a54a540b77d6d2764effe9a734f
Lin Tien-Li <tienli@LindeMacBook-Pro.local>
2023-07-10 10:24:48 +0800

    test sever hello world

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@
 const express = require( 'express' )
 const app = express()
 
+app.get("/", function(req, res) {
+    res.send("Hello world");
+});
+
 app.listen(3000, function() {
     console.log("server run on port 3000.");
 });
