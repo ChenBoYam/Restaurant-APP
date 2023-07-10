69de5c80ea0bed78bab0af059f6327fb47ca8a22
Lin Tien-Li <tienli@LindeMacBook-Pro.local>
2023-07-10 10:13:51 +0800

    test server

--- backend/server.js
+++ backend/server.js
@@ -1,57 +1,61 @@
-require( 'dotenv' ).config()
+// require( 'dotenv' ).config()
 const express = require( 'express' )
 const app = express()
-const path = require( 'path' )
-const { logger } = require( './middleware/logger' )
-const errorHandler = require( './middleware/errorHandler' )
-const cookieParser = require( 'cookie-parser' )
-const cors = require( 'cors' )
-const corsOptions = require( './config/corsOptions' )
-const connectDB = require( './config/dbConn' )
-const mongoose = require( 'mongoose' )
-const { logEvents } = require( './middleware/logger' )
-const PORT = process.env.PORT || 3500
 
-console.log( process.env.NODE_ENV )
+app.listen(3000, function() {
+    console.log("server run on port 3000.");
+});
+// const path = require( 'path' )
+// const { logger } = require( './middleware/logger' )
+// const errorHandler = require( './middleware/errorHandler' )
+// const cookieParser = require( 'cookie-parser' )
+// const cors = require( 'cors' )
+// const corsOptions = require( './config/corsOptions' )
+// const connectDB = require( './config/dbConn' )
+// const mongoose = require( 'mongoose' )
+// const { logEvents } = require( './middleware/logger' )
+// const PORT = process.env.PORT || 3500
 
-connectDB()
+// console.log( process.env.NODE_ENV )
 
-app.use( logger )
+// connectDB()
 
-app.use( cors( corsOptions ) )
+// app.use( logger )
 
-app.use( express.json() )
+// app.use( cors( corsOptions ) )
 
-app.use( cookieParser() )
+// app.use( express.json() )
 
-// express.static is a biuld in middleware
-app.use( '/', express.static( path.join( __dirname, 'public' ) ) )
+// app.use( cookieParser() )
 
-app.use( '/', require( './routes/root' ) )
-app.use('/users', require('./routes/userRoutes'))
+// // express.static is a biuld in middleware
+// app.use( '/', express.static( path.join( __dirname, 'public' ) ) )
 
-app.all( '*', ( req, res ) =>
-{
-    res.status( 404 )
-    if ( req.accepts( 'html' ) ) {
-        res.sendFile( path.join( __dirname, 'views', '404.html' ) )
-    } else if ( req.accepts( 'json' ) ) {
-        res.json( {
-            message: '404 Not Found'
-        } )
-    } else {
-        res.type( 'txt' ).send( '404 Not Found' )
-    }
-} )
+// app.use( '/', require( './routes/root' ) )
+// app.use('/users', require('./routes/userRoutes'))
 
-app.use( errorHandler )
+// app.all( '*', ( req, res ) =>
+// {
+//     res.status( 404 )
+//     if ( req.accepts( 'html' ) ) {
+//         res.sendFile( path.join( __dirname, 'views', '404.html' ) )
+//     } else if ( req.accepts( 'json' ) ) {
+//         res.json( {
+//             message: '404 Not Found'
+//         } )
+//     } else {
+//         res.type( 'txt' ).send( '404 Not Found' )
+//     }
+// } )
 
-mongoose.connection.once('open', () => {
-    console.log('MongoDB connected')
-    app.listen( PORT, () => console.log( `Server Running on port ${ PORT }` ) )
-})
+// app.use( errorHandler )
 
-mongoose.connection.on('error', err => {
-    console.log(err)
-    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
-})
+// mongoose.connection.once('open', () => {
+//     console.log('MongoDB connected')
+//     app.listen( PORT, () => console.log( `Server Running on port ${ PORT }` ) )
+// })
+
+// mongoose.connection.on('error', err => {
+//     console.log(err)
+//     logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+// })
