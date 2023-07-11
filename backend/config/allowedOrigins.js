8da2b64ee1b3dc74a5a68cce346f643caa9634d8
陳柏元 <c_boyuan@chenbaiyuandeMacBook-Pro.local>
2023-07-10 17:58:56 -0700

    Yuan

---
+++ backend/config/allowedOrigins.js
@@ -0,0 +1,9 @@
+const allowedOrigins = [
+    'http://localhost:3500',
+    'https://www.3mealfood.com',
+    'https://3mealfood.com',
+    'https://www.backend.3mealfood.com',
+    'https://backend.3mealfood.com'
+]
+
+module.exports = allowedOrigins
